f2a67111ce2fe7285d2656e886ee0b368ca5ffb0
fractal void <kammel@posteo.de>
2024-05-27 20:53:03 +0200

    training

---
+++ WPF_CustomMainWindow/NativeMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_CustomMainWindow
+{
+    internal class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        internal static extern bool GetMonitorInfo(nint hMonitor, MONITORINFO lpmi);
+
+        [DllImport("user32.dll")]
+        internal static extern nint MonitorFromWindow(nint hWnd, int flags);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    { public int x; public int y; }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMinTrackPosition;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class MONITORINFO
+    {
+        public int cbSize = Marshal.SizeOf(typeof(MINMAXINFO));
+        public RECT rcMonitor = new RECT();
+        public RECT rcWork = new RECT();
+        public int dwFlags = 0;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+}
---
+++ WPF_CustomMainWindow/ThemedWindowConverters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF_CustomMainWindow
+{
+    internal class MaximizeVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (ResizeMode)value == ResizeMode.NoResize ||
+                (ResizeMode)value == ResizeMode.CanMinimize ? Visibility.Collapsed : Visibility.Visible; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MinimizeVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (ResizeMode)value == ResizeMode.NoResize ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

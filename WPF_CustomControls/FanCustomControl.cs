f874f3f740314f80ab653d2acd21badde0a1e50d
fractal void <kammel@posteo.de>
2024-05-31 15:48:01 +0200

    training

---
+++ WPF_CustomControls/FanCustomControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CustomControls
+{
+
+    public class FanCustomControl : Control
+    {
+
+
+        public bool FanOn
+        {
+            get { return (bool)GetValue(FanOnProperty); }
+            set { SetValue(FanOnProperty, value); }
+        }
+
+        public static readonly DependencyProperty FanOnProperty =
+            DependencyProperty.Register("FanOn", typeof(bool), typeof(FanCustomControl), new PropertyMetadata(false));
+
+
+
+        static FanCustomControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FanCustomControl), new FrameworkPropertyMetadata(typeof(FanCustomControl)));
+        }
+    }
+}
---
+++ WPF_CustomControls/NavButton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CustomControls
+{
+
+    public class NavButton : ListBoxItem
+    {
+
+        public Uri Navlink
+        {
+            get { return (Uri)GetValue(NavlinkProperty); }
+            set { SetValue(NavlinkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NavlinkProperty =
+            DependencyProperty.Register("MyProperty", typeof(Uri), typeof(NavButton), new PropertyMetadata(null));
+
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Geometry), typeof(NavButton), new PropertyMetadata(null));
+
+
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+    }
+}
